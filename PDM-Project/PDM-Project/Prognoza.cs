4decf7cf3e0990363c75a90e599bd5ce41922f46
Radu Stoica <radux188@gmail.com>
2022-10-13 15:08:21 +0300

    create class + pages

---
+++ PDM-Project/PDM-Project/Prognoza.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDM_Project
+{
+	public class Prognoza
+	{
+		public int Id { get; set; }	
+		public int Minim { get; set; }	
+		public int Maxim { get; set; }	
+		public string Descriere { get; set; }
+		public DateTime Data { get; set; }	
+
+		public Prognoza() {}
+
+		
+	}
+}
---
+++ PDM-Project/PDM-Project/SearchPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace PDM_Project;
+
+public partial class SearchPage : ContentPage
+{
+	public SearchPage()
+	{
+		InitializeComponent();
+	}
+}
